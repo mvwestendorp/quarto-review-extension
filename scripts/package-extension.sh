f9f2f5cb2573011d8b1021a9e495846605c86ec2
Mathijs van Westendorp <mathijs@vanwestendorp.org>
2025-10-31 15:26:01 +0000

    fix: close option loop in packaging script

--- scripts/package-extension.sh
+++ scripts/package-extension.sh
@@ -18,10 +18,9 @@ while [[ $# -gt 0 ]]; do
   case "$1" in
     --skip-build)
       SKIP_BUILD=true
-      shift
       ;;
     -h|--help)
-      grep '^#' "$BASH_SOURCE" | sed 's/^# \{0,1\}//'
+      grep '^#' "${BASH_SOURCE[0]}" | sed 's/^# \{0,1\}//'
       exit 0
       ;;
     *)
@@ -29,7 +28,8 @@ while [[ $# -gt 0 ]]; do
       exit 1
       ;;
   esac
-end
+  shift
+done
 
 if ! $SKIP_BUILD; then
   pushd "$ROOT_DIR" >/dev/null
